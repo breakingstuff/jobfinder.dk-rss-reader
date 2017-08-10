354e6e9a90ee08b4dc80d1ac4edb6930ac41495a
breakingstuff <30905475+breakingstuff@users.noreply.github.com>
2017-08-10 18:18:12 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>.+</title>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+
+<?php
+
+   $rss = new DOMDocument();
+   $rss->load('https://www.jobfinder.dk/jobsrss/');
+   $feed = array();
+   foreach ($rss->getElementsByTagName('item') as $node) {
+      $item = array(
+                'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+                'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+                'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+                );
+      array_push($feed, $item);
+   }
+   $limit = 60;
+   for($x=0;$x<$limit;$x++) {
+      $title = str_replace(' & ', '&amp; ', $feed[$x]['title']);
+      $link = $feed[$x]['link'];
+      $description = $feed[$x]['desc'];
+      echo '<p><b><a href="'.$link.'" target="_blank">'.$title.'</a></b></p>';
+      echo '<p>'.$description.'</p>';
+      echo '<p><strong><em>'.$creator.'</em></strong></p>';
+   }
+?>
+</div>
+</body>
+</html>
